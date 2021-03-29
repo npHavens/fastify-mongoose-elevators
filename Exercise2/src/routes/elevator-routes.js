bf601ec7a06f97e1cf3cf763384885084cc1cb34
Nick Havens <npahavens@gmail.com>
2021-03-28 22:45:18 -0500

    doc: add Schema for POST elevators

--- Exercise2/src/routes/elevator-routes.js
+++ Exercise2/src/routes/elevator-routes.js
@@ -6,9 +6,37 @@ exports.elevatorRoutes = [
         url: '/api/elevators',
         handler: elevatorController.getElevators
      },
-     {
+     {  
          method: 'POST',
          url: '/api/buildings/:buildingId/elevators',
+         // Example of Request/Response Scheme generated in Swagger
+         schema: {
+            params: {
+                type: 'object',
+                properties: {
+                    buildingId: { type: 'string' }
+                }
+            },
+            body: {
+                type: 'object',
+                required: ['_id'],
+                properties: {
+                    _id: { type: 'string' }
+                }
+            },
+            response: {
+                200: {
+                    type: 'object',
+                    properties: {
+                        "currentFloor": { type: 'string' },
+                        "floorsToVisit": { type: 'array' },
+                        "_id": { type: 'string' },
+                        "buildingId": { type: 'number' },
+                        "__v": { type: 'number' }
+                    }
+                }
+            }
+         },
          handler: elevatorController.addElevator
      },
      {
